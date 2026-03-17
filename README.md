a34eaf3ed5a31530c8d0ee44873ba0f3c3537d60
lydvinebagaza <lydvine.bagaza@strathmore.edu>
2026-03-17 13:24:38 +0100

    main class 3

--- README.md
+++ README.md
@@ -1,18 +1,14 @@
 ## Getting Started
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+# Hospital Management System
 
-## Folder Structure
+## Description
+This project manages patients, doctors and appointments using OOP concepts.
 
-The workspace contains two folders by default, where:
+## How to Run
+1. Compile: javac *.java
+2. Run: java Main
 
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
-
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
-
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+## Exceptions Implemented
+- InputMismatchException handled using try-catch
+- Prevents program crash on invalid input
--- main.java
+++ main.java
@@ -1,27 +1,32 @@
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
 
-        Hospital hospital =
-                new Hospital("City Hospital", "Nairobi");
 
-        Patient p1 =
-                new Patient(1, "Perlita", "0700000000", "Malaria");
+         try { System.out.println("Enter patient ID:");
+int id = sc.nextInt();
+sc.nextLine(); 
 
-        Doctor d1 =
-                new Doctor(2, "Dr Monica", "0711111111",
-                        "Cardiology", 5000);
+System.out.println("Enter patient name:");
+String name = sc.nextLine();
 
-        Appointment ap1 =
-                new Appointment(101, "27 nov", p1, d1);
+System.out.println("Enter phone number:");
+String phone = sc.nextLine();
 
-        p1.admitPatient();
-        d1.diagnosePatient();
-        ap1.schedule();
+System.out.println("Enter illness:");
+String illness = sc.nextLine();
+Patient p1 = new Patient (id, name, phone, illness);
+p1.admitPatient();
+System.out.println("Payment:" +p1.calculatePayment ());
+                
+        } catch (Exception e) {
+                System.out.println("Invalid input");
+         } finally {
+                sc.close ();
+         }
+        }
 
-        System.out.println(p1.getDetails());
-        System.out.println("Payment: " + p1.calculatePayment());
-
-        hospital.generateReport();
-    }
+       
 }
